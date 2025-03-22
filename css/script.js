99ae94366a95b99f9af78c0a628011f976df69b0
Iytsuki <imitsukiichan6@gmail.com>
2025-03-23 00:49:21 +0800

    Update

---
+++ css/script.js
@@ -0,0 +1,33 @@
+const noButton = document.querySelector(".no-button");
+const container = document.querySelector(".container");
+
+// Function to move the "No" button
+function moveNoButton() {
+    let maxX = container.clientWidth - noButton.clientWidth - 20;
+    let maxY = container.clientHeight - noButton.clientHeight - 80; 
+
+    let newX = Math.max(0, Math.random() * maxX);
+    let newY = Math.max(0, Math.random() * maxY);
+
+    noButton.style.transform = `translate(${newX}px, ${newY}px)`;
+}
+
+// Move button on hover (desktop) or tap (mobile)
+noButton.addEventListener("mouseover", moveNoButton);
+noButton.addEventListener("touchstart", moveNoButton, { passive: true }); // Works on Android
+
+noButton.addEventListener("touchstart", (event) => {
+    event.preventDefault();
+    moveNoButton();
+}, { passive: false });
+
+// Function when clicking "Yes"
+function handleYesClick() {
+    // Change the GIF
+    
+    document.querySelector(".gif-container img").src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmVucW15NnlmNTEzN2N0Ymw0MDFid3Zyd29mbnc3aXF0dGlsZ3gwNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/wHUWuJWg6iRRQWTBd1/giphy.gif";    
+    alert("Great! Let's spend some time together!");
+    window.location.href = "html/stay_page.html";
+    
+    
+}
